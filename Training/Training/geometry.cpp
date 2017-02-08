6b72f28b6053793007a235c4026ddc8a6428c7ca
Adel Araji <adel.araji@youi.tv>
2017-02-08 15:05:35 -0500

    Asgt3-part1

--- Training/Training/geometry.cpp
+++ Training/Training/geometry.cpp
@@ -11,15 +11,124 @@
 #include <string>
 #include "geometry.h"
 
+//********************************************Point***************************************************
+Point::Point(int x, int y){
+    xCoordinate=x;
+    yCoordinate=y;
+}
+
+int Point::getX() const{
+    return xCoordinate;
+}
+
+int Point::getY() const{
+    return yCoordinate;
+}
+
+void Point::setX(const int x){
+    xCoordinate=x;
+}
+
+void Point::setY(const int y){
+    yCoordinate=y;
+}
+void Point::toString(){
+    cout<<"("<<this->getX()<<","<<this->getY()<<")"<<endl;
+}
+//********************************************PointArray**********************************************
+PointArray::PointArray(){
+    lengthOfArray=0;
+    points = new Point[0];
+}
+
+PointArray::PointArray(const Point points[], const int size){
+    lengthOfArray=size;
+    this->points=new Point[size];
+    for(int i=0;i<size;i++){
+        this->points[i]=points[i];
+    }
+}
+
+PointArray::PointArray(const PointArray &pv){
+    lengthOfArray=pv.lengthOfArray;
+    this->points=new Point[lengthOfArray];
+    for(int i=0;i<lengthOfArray;i++){
+        this->points[i] = pv.points[i];
+    }
+}
+
+void PointArray::resize(int newSize){
+    PointArray *temp = new PointArray(*this);
+    if (newSize<this->lengthOfArray) {
+        for(int i=0;i<newSize;i++){
+            temp->points[i]=this->points[i];
+        }
+        for(int i=newSize;i<this->lengthOfArray;i++){
+            this->points[i]=NULL;
+        }
+    }else{
+        for(int i=0;i<lengthOfArray;i++){
+            temp->points[i]=this->points[i];
+        }
+    }
+    this->lengthOfArray=newSize;
+    for(int i=0;i<lengthOfArray;i++){
+        this->points[i]=temp->points[i];
+    }
+    delete temp;
+}
+
+void PointArray::push_back(const Point &p){
+    this->points[lengthOfArray]=p;
+    lengthOfArray++;
+}
+
+void PointArray::insert(const int position, const Point &p){
+    for(int i=lengthOfArray;i>position;i--){
+        this->points[i]=this->points[i-1];
+    }
+    this->points[position]=p;
+    lengthOfArray++;
+}
+
+void PointArray::remove(const int pos){
+    for(int i=pos;i<lengthOfArray;i++){
+        this->points[i]=this->points[i+1];
+    }
+    this->points[lengthOfArray-1]=NULL;
+    lengthOfArray--;
+}
+
+const int PointArray::getSize() const{
+    return this->lengthOfArray;
+}
+
+void PointArray::clear(){
+    for(int i=0;i<lengthOfArray;i++){
+        this->points[i]=NULL;
+    }
+    this->lengthOfArray=0;
+}
+
+Point *PointArray::get(const int position){
+    if(this->lengthOfArray==0 || position>lengthOfArray-1 || position<0)
+        return NULL;
+    else{
+        return &this->points[position];
+    }
+}
+
+void PointArray::tostring(){
+    cout<<"*****"<<endl;
+    for(int i=0;i<lengthOfArray;i++){
+        cout<<"("<<this->points[i].getX()<<","<<points[i].getY()<<")"<<endl;
+    }
+    cout<<"*****"<<endl;
+}
+//*******************************************Polygon**************************************************
+
+
 
 int main() {
-    Point *p1 = new Point(0,0);
-    Point p2 = *new Point(0,5);
-    Point p3 = *new Point(2,0);
-    Point p4 = *new Point(2,5);
-    
-    Rectangle *rect = new Rectangle(p1,p2,p3,p4);
-    
-//    cout<<"The area is: "<<rect.calculateArea()<<endl;
-    cout<<"HI"<<endl;
+ 
 }
